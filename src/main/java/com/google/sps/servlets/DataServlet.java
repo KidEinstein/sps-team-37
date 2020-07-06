8f3263ddb8091912943596e6ed2706959c742634
gracieorozco <orozco.gracie.m@gmail.com>
2020-07-05 22:32:08 -0700

    Update graph to parse data using a GET request

--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -17,27 +17,52 @@ package com.google.sps.servlets;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.gson.Gson;
+import com.google.sps.data.Journal;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that stores journal entry data from the html form into Datastore */
+/** Servlet that stores journal entry data from the html form into Datastore*/
 @WebServlet("/my-data-url")
 public class DataServlet extends HttpServlet {
 
+  ArrayList<Journal> journalArrayList = new ArrayList<Journal>();
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    // Datastore query for journal entries ordered by time in descending order. 
+    Query journalQuery = new Query("Journal").addSort("timestamp", SortDirection.DESCENDING);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    PreparedQuery journalResults = datastore.prepare(journalQuery);
+    
+    // Loop through the queried results and create a Journal object to add to an ArrayList. 
+    for (Entity journalEntity : journalResults.asIterable()) {
+        long moodValue = (long) journalEntity.getProperty("mood-scale");
+        long timestamp = (long) journalEntity.getProperty("timestamp");
+        Journal journal = new Journal(moodValue, timestamp);
+        journalArrayList.add(journal);
+    }
+
+    // Convert the ArrayList of Journals into JSON format. 
+    response.setContentType("application/json");
+    Gson gson = new Gson();
+    String journalJson = gson.toJson(journalArrayList);
+    response.getWriter().println(journalJson);
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Get the input from the form.
+    // Get the input and time from the form.
     String moodScaleString = request.getParameter("mood");
+    long timestamp = System.currentTimeMillis();
 
     // Convert the mood input to an int.
     int moodScale = Integer.parseInt(moodScaleString);
@@ -45,6 +70,7 @@ public class DataServlet extends HttpServlet {
     //Create journal entity with mood, journal entry, and song properties
     Entity journalEntity = new Entity("Journal");
     journalEntity.setProperty("mood-scale", moodScale);
+    journalEntity.setProperty("timestamp", timestamp);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(journalEntity);
--- src/main/webapp/graphs_script.js
+++ src/main/webapp/graphs_script.js
@@ -3,30 +3,27 @@ google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawGraph);
 
 function drawGraph() {
-  const data = new google.visualization.DataTable();
-  data.addColumn('string', 'Date');
-  data.addColumn('number', 'Mood Rating');
-        data.addRows([
-          ['6/1/20', 3],
-          ['6/7/20', 5],
-          ['6/14/20', 6],
-          ['6/21/20', 8],
-          ['6/28/20', 3],
-          ['7/1/20', 9],
-          ['7/7/20', 5],
-          ['7/14/20', 7],
-          ['7/21/20', 2],
-          ['7/28/20', 3],
-        ]);
+  // Fetches Journal entry data from the DataServlet. 
+  fetch('/my-data-url').then(response => response.json()).then((journalData) => {
 
-  const options = {
-    'title': 'Mood Rating',
-    vAxis: {
-        ticks: [0, 2, 4, 6, 8, 10]
-    }
-  };
+    // Add the data from the journal entry into the data table. 
+    const data = new google.visualization.DataTable();
+    data.addColumn('string', 'Date');
+    data.addColumn('number', 'Mood Rating');
+    Object.keys(journalData).forEach((journal) => {
+        var date = new Date(journalData[journal].timestamp).toDateString();
+        data.addRow([date, journalData[journal].moodValue]);
+    });
 
-  const chart = new google.visualization.LineChart(
-      document.getElementById('chart-container'));
-  chart.draw(data, options);
+    // Additional attributes to update the y-axis. 
+    const options = {
+        vAxis: {
+            ticks: [0, 2, 4, 6, 8, 10]
+        }
+    };
+
+    // Creates the graph in the following element in the HTML file. 
+    const chart = new google.visualization.LineChart(document.getElementById('graph-div'));
+    chart.draw(data, options);
+  });
 }
