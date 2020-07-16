a2245e9ecc8cfa489999cac9972e3e8eb1ca5e52
Frederick Santiago <frsantiago2001@gmail.com>
2020-07-16 00:54:43 -0400

    Add emoji to datastore based on mood scale

--- src/main/java/com/google/sps/data/Journal.java
+++ src/main/java/com/google/sps/data/Journal.java
@@ -8,13 +8,15 @@ public final class Journal {
   private final long moodValue;
   private final String songTitle;
   private final String artistName;
+  private final String emoji;
   private final long timestamp;
 
-  public Journal( String textEntry, long moodValue, String songTitle,  String artistName, long timestamp ) {
+  public Journal( String textEntry, long moodValue, String songTitle,  String artistName, String emoji, long timestamp ) {
     this.textEntry = textEntry;
     this.moodValue = moodValue;
     this.songTitle = songTitle;
     this.artistName = artistName;
+    this.emoji = emoji;
     this.timestamp = timestamp;
   }
 }
