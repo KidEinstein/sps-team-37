e9ac6ce2b35b63067d8c1698d93ae0b2653c8564
Diego <dprado981@gmail.com>
2020-07-18 18:29:36 -0700

    Only show entries of the logged in user and add server-side validation of login status

--- src/main/java/com/google/sps/data/Journal.java
+++ src/main/java/com/google/sps/data/Journal.java
@@ -1,7 +1,7 @@
 
 package com.google.sps.data;
 
-/** A Journal object containing the text, mood value, song, artist, and time. */
+/** A Journal object containing the text, mood value, song, artist, time, and email. */
 public final class Journal {
 
   private final String textEntry;
@@ -9,12 +9,14 @@ public final class Journal {
   private final String songTitle;
   private final String artistName;
   private final long timestamp;
+  private final String email;
 
-  public Journal( String textEntry, long moodValue, String songTitle,  String artistName, long timestamp ) {
+  public Journal( String textEntry, long moodValue, String songTitle,  String artistName, long timestamp, String email ) {
     this.textEntry = textEntry;
     this.moodValue = moodValue;
     this.songTitle = songTitle;
     this.artistName = artistName;
     this.timestamp = timestamp;
+    this.email = email;
   }
 }
--- src/main/java/com/google/sps/servlets/DataServlet.java
+++ src/main/java/com/google/sps/servlets/DataServlet.java
@@ -20,6 +20,11 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
 import com.google.sps.data.Journal;
 
@@ -37,14 +42,24 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     
-    // An ArrayList to store Journal objects. 
-    ArrayList<Journal> journalArrayList = new ArrayList<Journal>();
-
+    // If a user is not signed in, then don't retreive any entries
+    UserService userService = UserServiceFactory.getUserService();
+    User currentUser = userService.getCurrentUser();
+    if (currentUser == null) { 
+      // No user is logged in
+      return;
+    }
     // Datastore query for journal entries ordered by time in ascending order. 
-    Query journalQuery = new Query("Journal").addSort("timestamp", SortDirection.ASCENDING);
+    Query journalQuery = new Query("Journal")
+                  .addFilter("email", FilterOperator.EQUAL, currentUser.getEmail())
+                  .addSort("timestamp", SortDirection.ASCENDING);
+
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery journalResults = datastore.prepare(journalQuery);
     
+    // An ArrayList to store Journal objects. 
+    ArrayList<Journal> journalArrayList = new ArrayList<Journal>();
+
     // Loop through the queried results and create a Journal object to add to an ArrayList. 
     for (Entity journalEntity : journalResults.asIterable()) {
         String textEntry = (String) journalEntity.getProperty("text");
@@ -52,8 +67,9 @@ public class DataServlet extends HttpServlet {
         String songTitle = (String) journalEntity.getProperty("song");
         String artistName = (String) journalEntity.getProperty("artist");
         long timestamp = (long) journalEntity.getProperty("timestamp");
+        String email = (String) journalEntity.getProperty("email");
 
-        Journal journal = new Journal(textEntry, moodValue, songTitle, artistName, timestamp);
+        Journal journal = new Journal(textEntry, moodValue, songTitle, artistName, timestamp, email);
         journalArrayList.add(journal);
     }
 
@@ -66,12 +82,21 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Only allow to save if a user is logged in
+    UserService userService = UserServiceFactory.getUserService();
+    User currentUser = userService.getCurrentUser();
+    if (currentUser == null) {
+      // No user is logged in
+      return;
+    }
+
     // Get the input and time from the form.
     String textEntryString = request.getParameter("text");
     String moodScaleString = request.getParameter("mood");
     String songEntryString = request.getParameter("song");
     String artistEntryString = request.getParameter("artist");
     long timestamp = System.currentTimeMillis();
+    String email = currentUser.getEmail();
 
     // Ensure that form is filled out before saving to datastore
     if (textEntryString != null && !textEntryString.isEmpty()) {
@@ -85,6 +110,7 @@ public class DataServlet extends HttpServlet {
       journalEntity.setProperty("song", songEntryString);
       journalEntity.setProperty("artist", artistEntryString);
       journalEntity.setProperty("timestamp", timestamp);
+      journalEntity.setProperty("email", email);
 
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
       datastore.put(journalEntity);
